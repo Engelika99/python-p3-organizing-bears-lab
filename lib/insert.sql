3f4a27a3c9411468cf577f3b37d4f3cb645ff8f5
Alexis Beauregard <mariposa_lex99@yahoo.com>
2023-08-10 17:13:06 -0400

    Practice select and insert

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, sex, age) VALUES ( 'Mr. Chocolate', 'M', 20); 
+INSERT INTO bears (name, sex, age, temperament) VALUES ( 'Rowdy', 'M', 10, 'Excitable'); 
+INSERT INTO bears (name, sex, age, color) VALUES ( 'Tabitha', 'F', 27, 'White'); 
+INSERT INTO bears (name, sex, age, color, temperament, alive) VALUES ( 'Sergeant Brown', 'M', 68, 'Brown', 'Lazy', 1 ); 
+INSERT INTO bears (name, sex, age) VALUES ( 'Melissa', 'F', 3); 
+INSERT INTO bears (name, sex, age, temperament, alive) VALUES ( 'Grinch', 'M', 87, 'Grumpy', 1); 
+INSERT INTO bears (name, sex, age) VALUES ( 'Wendy', 'F', 56); 
+INSERT INTO bears (name, sex, age, color, temperament, alive) VALUES ( NULL, 'F', 15, 'Black', 'Shy', 1); 
